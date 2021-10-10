3e316cf4005d290676d5d381e97c5854e3776469
duncanjake1 <jacobsenduncan@gmail.com>
2021-10-10 13:40:44 -0600

    made donate page suck less

--- src/components/Donate.jsx
+++ src/components/Donate.jsx
@@ -9,33 +9,60 @@ const Donate = () => {
         <h2>Making and maintaining webistes is hard work</h2>
         <p>But we love it</p>
       </div>
-      <div className="inner-div flex-container donate-container">
+      <div
+        className="inner-div flex-container donate-container"
+        style={{ flexDirection: "column" }}
+      >
         <div className="donate-item">
-          <h2>Want to donate to us?</h2>
-          <p>
-            This website is not currently in a useable state, but we are
-            determined to get it there. If you like the goal of this site and
-            feel inclined to throw us a few bucks, we would really appriciate
-            it!
-          </p>
-          <p>(donation ability is not currently active)</p>
+          <h2 className="donation-header">Want to donate to us?</h2>
+          <div
+            className="secondary-inner-div responsive-resize"
+            style={{ width: "800px" }}
+          >
+            <p style={{ color: "red" }}>
+              donation ability is not currently active
+            </p>
+            <p>
+              Before we allow the ability to donate to us, we want to make sure
+              this site is in a great state. But don't let that stop you from
+              donating to the community! Check out some of the sources we rely
+              on, and consider throwing them a couple bucks for everything they
+              do.
+            </p>
+          </div>
         </div>
         <div className="donate-item">
-          <h2>Don't want to donate to us?</h2>
-          <p>
-            No worries! But our site relies on a number of other great
-            organizations. Please consider supporting them for everything they
-            provide.
-          </p>
-          <ul>
-            <li>National Avalanche Center</li>
-            <li>Montana Snow Report</li>
-            <li>Colorado Avalanche Organization</li>
-          </ul>
+          <h2 className="donation-header">Don't want to donate to us?</h2>
+          <div
+            className="secondary-inner-div responsive-resize"
+            style={{ width: "1000px" }}
+          >
+            <p>
+              No worries! But our site relies on a number of other great
+              organizations. Please consider supporting them for everything they
+              provide.
+            </p>
+            <ul>
+              <a href="https://www.americanavalancheassociation.org/donate" target="_blank" rel="noreferrer">
+                <li className="responsive-li">
+                  American Avalanche Association
+                </li>
+              </a>
+              <a href="https://avtraining.org/donate-3/" target="_blank" rel="noreferrer">
+                <li className="responsive-li">
+								American Institute for Avalanche Research
+							</li>
+              </a>
+              <a href="https://charity.gofundme.com/o/en/campaign/powder-blast-2020" target="_blank" rel="noreferrer">
+                <li className="responsive-li">
+									Gallatin National Forest Avalanche Center
+                </li>
+              </a>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
   );
 };
 export default Donate;
-
